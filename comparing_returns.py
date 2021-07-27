c3dba7bb601d62524f225b1a5b239ba1aa64d324
jackkanehann <63209956+jackkanehann@users.noreply.github.com>
2021-07-27 08:24:00 -0400

    Add files via upload

--- comparing_returns.py
+++ comparing_returns.py
@@ -1,7 +1,12 @@
 import pandas as pd
+import numpy as np
 import pandas_datareader as web
 import datetime
-import numpy as np
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+import seaborn
+from scipy.stats import norm
+from tabulate import tabulate
 import pyEX as p
 iex_sandbox_key = 'Tpk_ec52cb37b61542d8b350bf9df129fcb5'
 
@@ -23,13 +28,8 @@ currentPortfolio.display_summary()
 portfolio_df = pd.read_csv(currentPortfolio.path + currentPortfolio.file_name)
 portfolio_df = transformCSV(portfolio_df)
 
-##check df transformed correctly
-print(portfolio_df.head(10))
-
 tickerList = portfolio_df['Symbol'].tolist()
-print (tickerList)
 weightList = portfolio_df['Weight'].tolist()
-print(weightList)
 
 end = datetime.datetime(2021,7,1)
 start = datetime.datetime(2021,6,1)
@@ -40,23 +40,35 @@ def PortfolioWeeklyReturns(symbolList, weightList, IEXobject):
     i = 0
     for symbol in symbolList:
         symbolReturns = getWeeklyReturns(symbol, IEXobject)
-        ##print(symbolReturns)
         for profit in symbolReturns[1]:
             profit = profit * weightList[i]
         symbolReturns = symbolReturns[1]
-        ##print(symbolReturns)
         i=i+1
-
         returnList = returnList + symbolReturns
 
-    print("\n"+"\n"+"List of pertfolio returns is ", returnList)
+    print("\n"+"List of weekly portfolio returns is "+"\n", returnList)
 
+    return returnList
+
+def PortfolioDailyReturns(symbolList, weightList, IEXobject):
+    returnList = []
+    i = 0
+    for symbol in symbolList:
+        symbolReturns = getDailyReturns(symbol, IEXobject)
+        for profit in symbolReturns[1]:
+            profit = profit * weightList[i]
+        symbolReturns = symbolReturns[1]
+        i=i+1
+        returnList = returnList + symbolReturns
+
+    print("\n"+"List of daily portfolio returns is "+"\n", returnList)
 
     return returnList
 
 def getWeeklyReturns(symbol, IEXobject):
     df = IEXobject.chartDF(symbol=symbol, timeframe='5y', interval = 5)[['close', 'volume']]
     maxIndex = len(df['close']) -1
+
     i=0
     profitList = []
     returnList = [symbol, profitList]
@@ -68,10 +80,24 @@ def getWeeklyReturns(symbol, IEXobject):
 
     return returnList
 
+def getDailyReturns(symbol, IEXobject):
+    df = IEXobject.chartDF(symbol=symbol, timeframe='5y', interval = 1)[['close', 'volume']]
+    maxIndex = len(df['close']) -1
+
+    i=0
+    profitList = []
+    returnList = [symbol, profitList]
+
+    while i < maxIndex:
+        profit = df['close'][i] / df['close'][i+1] -1
+        profitList.append(profit)
+        i = i+1
+
+    return returnList
 
 ##change from ticker objects to series objects
-def ReturnStatistics(ticker1, ticker2, IEXobject):
-##for finding the return statistics of a single ticker against a given benchmark
+def TickerReturnStatistics(ticker1, ticker2, IEXobject):
+##for finding the return statistics of a single ticker against a given benchmark, need to separate series of returns from symbol key in dictionary
     testSeries = getWeeklyReturns(ticker1, IEXobject)
     testSeries = testSeries[1]
     testSeries = pd.Series(testSeries)
@@ -80,24 +106,74 @@ def ReturnStatistics(ticker1, ticker2, IEXobject):
     benchmark = benchmark[1]
     benchmark = pd.Series(benchmark)
 
-  ##return calculations, need to separate series of returns from symbol key in dictionary
+  ##return calculations
     print("Correlation is ", testSeries.corr(benchmark, method = 'pearson', min_periods = 10))
     print("Covariance is ", testSeries.cov(benchmark, min_periods = 10, ddof = 1))
 
     return
 
+##change from ticker objects to series objects
+def PortfolioReturnStatistics(portfolioReturns, ticker2, IEXobject):
+##for finding the return statistics of a single ticker against a given benchmark
+    testSeries = pd.Series(portfolioReturns)
+
+    ## need to separate series of returns from symbol key in dictionary
+    benchmark = getWeeklyReturns(ticker2, IEXobject)
+    benchmark = benchmark[1]
+    benchmark = pd.Series(benchmark)
+
+  ## VGSH is Vanguard Short-Term Treasury Index Fund ETF, a suitable proxy for the risk-free rate of return over a given period
+    riskFreeSeries = getWeeklyReturns("VGSH", IEXobject)
+    riskFreeSeries = riskFreeSeries[1]
+    riskFreeSeries = pd.Series(riskFreeSeries)
+
+    mean = np.mean(testSeries)
+    std_dev = np.std(testSeries)
+    VaR90 = norm.ppf(1-.90, mean, std_dev)
+    VaR95 = norm.ppf(1-.95, mean, std_dev)
+    VaR99 = norm.ppf(1-.99, mean, std_dev)
+
+  ##return calculations
+    print("Portfolio correlation is ", testSeries.corr(benchmark, method = 'pearson', min_periods = 10))
+    print("Portfolio covariance is ", testSeries.cov(benchmark, min_periods = 10, ddof = 1))
+    print(tabulate([['Confidence Level', 'Value at Risk'], ['90%', VaR90], ['95%', VaR95], ['99%', VaR99]]))
+
+    ##graph returns
+    testSeries.hist(bins=40, histtype='stepfilled', alpha=0.5)
+    x = np.linspace(mean-3*std_dev, mean + 3*std_dev, 100)
+    plt.plot(x, norm.pdf(x, mean, std_dev),"r")
+    plt.show()
+
+    """##graph differenced returns -- how to difference pd.Series()?
+    ##differncedSeries = pd.Series(ksjdhfk)
+    mean2 = np.mean(testSeries)
+    std_dev2 = np.std(testSeries)
+
+    xx = np.linspace(mean2-3*std_dev2, mean2 + 3*std_dev2, 100)
+    plt.plot(xx, norm.pdf(x, mean2, std_dev2),"r")
+    plt.show()"""
+
+
+
+    return
 
 ##IEX Stock Price Return sourcing using pyEX library
 c= p.Client(api_token = 'Tpk_ec52cb37b61542d8b350bf9df129fcb5', version = 'sandbox')
-##ReturnStatistics("AAPL", "SPY", c)
-PortfolioWeeklyReturns(tickerList, weightList, c)
+##TickerReturnStatistics("AAPL", "SPY", c)
+returnList = PortfolioWeeklyReturns(tickerList, weightList, c)
+PortfolioReturnStatistics(returnList ,"SPY", c)
 
 
 
 
 
 
-##find weekly returns for companies in portfolio (Done) -->weighted average to find for portfolio
+##find weekly returns for companies in portfolio (Done) -->weighted average to find for portfolio (done)
 ##regress/chart portfolio wwekly returns against sp500
-##run portfolio return correlation to S&P500
-##Out portfolio weekly VaR
+##run portfolio return correlation to S&P500 (done)
+##Out portfolio weekly VaR (done)
+
+##clean up portfolio output
+##run from a __main__: code segment
+
+##logarithmic returns vs ordinary returns -- probably make the change soon
